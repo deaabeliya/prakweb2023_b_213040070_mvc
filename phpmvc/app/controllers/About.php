2eba56f1574356b888e0554febfacf9ed278f902
deaabeliya <dblyaaaa@gmail.com>
2023-10-20 17:11:19 +0700

    video 5 View

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,12 +1,22 @@
 <?php
 
-class About {
-    public function index($nama = 'Dea', $pekerjaan = 'Mahasiswa')
-    {
-        echo "Halo, nama saya $nama, saya seorang $pekerjaan";
+    class About extends Controller{
+        public function index($nama = "Radhitya", $pekerjaan = "Mahasiswa", $umur =19) {
+            $data['nama'] = $nama;
+            $data['pekerjaan'] = $pekerjaan;
+            $data['umur'] = $umur;
+            $data['judul'] = 'About Me';
+            $this->view('templates/header', $data);
+            $this->view('about/index', $data);
+            $this->view('templates/footer');
+        }
+
+        public function page() {
+            $data['judul'] = 'Pages';
+            $this->view('templates/header', $data);
+            $this->view('about/page');
+            $this->view('templates/footer');
+        }
     }
-    public function page()
-    {
-        echo 'About/page';
-    }
-}
+
+?>
